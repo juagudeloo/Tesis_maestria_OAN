891f2e86850c24a02cccd805a4bcce64d1638247
juanplanetai <jesteban.agudelo@planetai.space>
2025-02-12 16:50:44 +0100

    first experiment branch

--- Experiment.py
+++ Experiment.py
@@ -20,15 +20,25 @@ def main():
                  "085000", "090000"
                  ]
     
-    
+    #Load data
+    atm_data, stokes_data, mags_names = load_training_data(filenames)
     
     # Setup device agnostic code
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Tensors stored in: {device}")
     
+    # Create dataset and dataloaders
+    train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
+                       atm_data = atm_data,
+                       device = device,
+                       batch_size = 80)
+    
+    ### MODEL CREATION ###
+    hidden_units = [72, 1024, 2048, 4096]
+    
     ### Checking linear vs convolutional 1d models ###
     
-    epochs = 10
+    test_epochs = [10, 20]
     model_types = [
         "simple_linear", "simple_cnn1d_36channels", 
                    "simple_cnn1d_4channels"]
@@ -37,75 +47,63 @@ def main():
     
     
     #1. Loop through model types
-    
-    # Create dataset and dataloaders
-    #Load data
-    
-    n_wl_points = [36, 58, 90, 112]
-    
-    for n_wl in n_wl_points:
-        print(f"Training for {n_wl} wavelength points")
-        atm_data, stokes_data, _ = load_training_data(filenames)
-        train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
+    for m_type in model_types:
+        print(f"Training {m_type} models")
+        if m_type == "simple_linear":
+            train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
                         atm_data = atm_data,
                         device = device,
-                        batch_size = 80)
-        for m_type in model_types:
-            print(f"Training {m_type} models")
-            if m_type == "simple_linear":
-                train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
-                            atm_data = atm_data,
-                            device = device,
-                            batch_size = 80,
-                            linear = True)
-            elif m_type == "simple_cnn1d_36channels":
-                train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
-                            atm_data = atm_data,
-                            device = device,
-                            batch_size = 80,
-                            stokes_as_channels=False,
-                            linear = False)
-            elif m_type == "simple_cnn1d_4channels":
-                train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
-                            atm_data = atm_data,
-                            device = device,
-                            batch_size = 80,
-                            stokes_as_channels=True,
-                            linear = False)
-            #Creating the model
-            if m_type == "simple_linear":
-                hu = 2048
-                model = SimpleLinearModel(36*4,6*20,hidden_units=hu).to(device)
-            elif m_type == "simple_cnn1d_36channels":
-                hu = 1024
-                model = SimpleCNN1DModel(36,6*20,hidden_units=hu, signal_length=4).to(device)
-            elif m_type == "simple_cnn1d_4channels":
-                hu = 72
-                model = SimpleCNN1DModel(4,6*20,hidden_units=hu, signal_length=36).to(device)
-            model = model.float()
-            #Loss function
-            loss_fn = nn.MSELoss() # this is also called "criterion"/"cost function" in some places
+                        batch_size = 80,
+                        linear = True)
+        elif m_type == "simple_cnn1d_36channels":
+            train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
+                        atm_data = atm_data,
+                        device = device,
+                        batch_size = 80,
+                        stokes_as_channels=False,
+                        linear = False)
+        elif m_type == "simple_cnn1d_4channels":
+            train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
+                        atm_data = atm_data,
+                        device = device,
+                        batch_size = 80,
+                        stokes_as_channels=True,
+                        linear = False)
+        #2. Loop through hidden units
+        for hu in hidden_units:
+            #3. Loop throuch epochs
+            for epochs in test_epochs:
+                #Creating the model
+                if m_type == "simple_linear":
+                    model = SimpleLinearModel(36*4,6*20,hidden_units=hu).to(device)
+                elif m_type == "simple_cnn1d_36channels":
+                    model = SimpleCNN1DModel(36,6*20,hidden_units=hu, signal_length=4).to(device)
+                elif m_type == "simple_cnn1d_4channels":
+                    model = SimpleCNN1DModel(4,6*20,hidden_units=hu, signal_length=36).to(device)
+                model = model.float()
+                #Loss function
+                loss_fn = nn.MSELoss() # this is also called "criterion"/"cost function" in some places
 
-            #Optimizer
-            optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
-            #Train model
-            train(model=model,
-                train_dataloader=train_dataloader,
-                test_dataloader=test_dataloader, 
-                optimizer=optimizer,
-                loss_fn=loss_fn,
-                epochs=epochs,
-                device=device,
-                writer=create_writer(experiment_name=str(hu)+"_hidden_units",
-                                    model_name=m_type,
-                                    extra=f"{epochs}_epochs"))
-            
-            #Save the model to file so we can get back the best model
-            save_filepath = f"{m_type}_{hu}_hidden_units_{epochs}_epochs.pth"
-            save_model(model=model,
-                    target_dir="models",
-                    model_name=save_filepath)
-            print("-"*50 + "\n")
+                #Optimizer
+                optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
+                #Train model
+                train(model=model,
+                    train_dataloader=train_dataloader,
+                    test_dataloader=test_dataloader, 
+                    optimizer=optimizer,
+                    loss_fn=loss_fn,
+                    epochs=epochs,
+                    device=device,
+                    writer=create_writer(experiment_name=str(hu)+"_hidden_units",
+                                        model_name=m_type,
+                                        extra=f"{epochs}_epochs"))
+                
+                #Save the model to file so we can get back the best model
+                save_filepath = f"{m_type}_{hu}_hidden_units_{epochs}_epochs.pth"
+                save_model(model=model,
+                        target_dir="models",
+                        model_name=save_filepath)
+                print("-"*50 + "\n")
 
             
 
