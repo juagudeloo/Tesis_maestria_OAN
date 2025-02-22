6080782cf6fc5f3a21754e24d80f3b32c769bb2c
juanplanetai <jesteban.agudelo@planetai.space>
2025-02-22 17:48:17 +0100

    test plot for the logtau atmosphere magnitudes

--- modules/nn_models.py
+++ modules/nn_models.py
@@ -8,11 +8,13 @@ class LinearModel(nn.Module):
         self.simple_model = nn.Sequential(
         nn.Linear(in_features = in_shape, out_features = hidden_units),
         nn.ReLU(),
-        nn.Linear(in_features = in_shape, out_features = hidden_units),
+        nn.Linear(in_features = hidden_units, out_features = hidden_units),
         nn.ReLU(),
-        nn.Linear(in_features = in_shape, out_features = hidden_units),
+        nn.Linear(in_features = hidden_units, out_features = hidden_units),
         nn.ReLU(),
-        nn.Linear(in_features = in_shape, out_features = hidden_units),
+        nn.Linear(in_features = hidden_units, out_features = hidden_units),
+        nn.ReLU(),
+        nn.Linear(in_features = hidden_units, out_features = hidden_units),
         nn.ReLU(),
         nn.Linear(in_features = hidden_units, out_features = out_shape)
         )
@@ -34,11 +36,11 @@ class CNN1DModel(nn.Module):
         self.simple_conv = nn.Sequential(
         nn.Conv1d(in_channels=in_shape, out_channels=hidden_units, kernel_size = kernel_size, stride=stride, padding=padding),
         nn.ReLU(),
-        nn.Conv1d(in_channels=in_shape, out_channels=hidden_units*2, kernel_size = kernel_size, stride=stride, padding=padding),
+        nn.Conv1d(in_channels=hidden_units, out_channels=hidden_units*2, kernel_size = kernel_size, stride=stride, padding=padding),
         nn.ReLU(),
-        nn.Conv1d(in_channels=in_shape, out_channels=hidden_units*4, kernel_size = kernel_size, stride=stride, padding=padding),
+        nn.Conv1d(in_channels=hidden_units*2, out_channels=hidden_units*4, kernel_size = kernel_size, stride=stride, padding=padding),
         nn.ReLU(),
-        nn.Conv1d(in_channels=in_shape, out_channels=hidden_units*8, kernel_size = kernel_size, stride=stride, padding=padding),
+        nn.Conv1d(in_channels=hidden_units*4, out_channels=hidden_units*8, kernel_size = kernel_size, stride=stride, padding=padding),
         nn.ReLU(),
         nn.Flatten(),
         nn.Dropout(p=0.5, inplace=False),
