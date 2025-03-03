a45f4357dc31cd3be72c89a5d3729c1937b7b3df
Juan Esteban <juagudeloo@unal.edu.co>
2025-03-03 04:49:32 -0500

    first experiment generate and experiment scripts

---
+++ Experiment.py
@@ -0,0 +1,112 @@
+import sys
+
+from timeit import default_timer as timer 
+
+import torch
+from torch import nn
+
+#MODULES IMPORT
+sys.path.append("../modules")
+from modules.data_utils import load_training_data, create_dataloaders
+from modules.nn_models import SimpleLinearModel, SimpleCNN1DModel
+from modules.train_test_utils import train, set_seeds, create_writer, save_model
+
+
+def main():
+    
+    ### DATA LOADING ###
+    #filenames to be readed for creating the dataset
+    filenames = ["080000", 
+                 "085000", "090000"
+                 ]
+    
+    #Load data
+    atm_data, stokes_data, _ = load_training_data(filenames)
+    
+    # Setup device agnostic code
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Tensors stored in: {device}")
+    
+    # Create dataset and dataloaders
+    train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
+                       atm_data = atm_data,
+                       device = device,
+                       batch_size = 80)
+    
+    ### MODEL CREATION ###
+    hidden_units = [72, 1024, 2048, 4096]
+    
+    ### Checking linear vs convolutional 1d models ###
+    
+    test_epochs = [10, 20]
+    model_types = [
+        "simple_linear", "simple_cnn1d_36channels", 
+                   "simple_cnn1d_4channels"]
+    lr = 1e-3
+    
+    
+    
+    #1. Loop through model types
+    for m_type in model_types:
+        print(f"Training {m_type} models")
+        if m_type == "simple_linear":
+            train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
+                        atm_data = atm_data,
+                        device = device,
+                        batch_size = 80,
+                        linear = True)
+        elif m_type == "simple_cnn1d_36channels":
+            train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
+                        atm_data = atm_data,
+                        device = device,
+                        batch_size = 80,
+                        stokes_as_channels=False,
+                        linear = False)
+        elif m_type == "simple_cnn1d_4channels":
+            train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
+                        atm_data = atm_data,
+                        device = device,
+                        batch_size = 80,
+                        stokes_as_channels=True,
+                        linear = False)
+        #2. Loop through hidden units
+        for hu in hidden_units:
+            #3. Loop throuch epochs
+            for epochs in test_epochs:
+                #Creating the model
+                if m_type == "simple_linear":
+                    model = SimpleLinearModel(36*4,6*20,hidden_units=hu).to(device)
+                elif m_type == "simple_cnn1d_36channels":
+                    model = SimpleCNN1DModel(36,6*20,hidden_units=hu, signal_length=4).to(device)
+                elif m_type == "simple_cnn1d_4channels":
+                    model = SimpleCNN1DModel(4,6*20,hidden_units=hu, signal_length=36).to(device)
+                model = model.float()
+                #Loss function
+                loss_fn = nn.MSELoss() # this is also called "criterion"/"cost function" in some places
+
+                #Optimizer
+                optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
+                #Train model
+                train(model=model,
+                    train_dataloader=train_dataloader,
+                    test_dataloader=test_dataloader, 
+                    optimizer=optimizer,
+                    loss_fn=loss_fn,
+                    epochs=epochs,
+                    device=device,
+                    writer=create_writer(experiment_name=str(hu)+"_hidden_units",
+                                        model_name=m_type,
+                                        extra=f"{epochs}_epochs"))
+                
+                #Save the model to file so we can get back the best model
+                save_filepath = f"{m_type}_{hu}_hidden_units_{epochs}_epochs.pth"
+                save_model(model=model,
+                        target_dir="models",
+                        model_name=save_filepath)
+                print("-"*50 + "\n")
+
+            
+
+
+if __name__ == "__main__":
+    main()
---
+++ Generate.py
@@ -0,0 +1,145 @@
+
+from pathlib import Path
+import torch
+import numpy as np
+
+#MODULES IMPORT
+import sys
+sys.path.append("../modules")
+from modules.data_utils import load_data_cubes
+from modules.nn_models import SimpleLinearModel, SimpleCNN1DModel
+from modules.train_test_utils import charge_weights, generate_results, descale_atm, plot_surface_generated_atm, plot_od_generated_atm, plot_density_bars
+
+
+def main():
+    
+    
+    #########################################################################################
+    # Preprocessing
+    #########################################################################################
+    
+    # Filenames of the snapshots to be calculated
+    filenames = ["175000"]
+    
+    # Load data
+    atm_data, stokes_data, mags_names, phys_maxmin = load_data_cubes(filenames)
+    
+    stokes_data_linear = np.reshape(np.copy(stokes_data[0]), (stokes_data[0].shape[0]*stokes_data[0].shape[1], stokes_data[0].shape[2]*stokes_data[0].shape[3]))
+    #----------------------------------------------
+    stokes_data_cnn_36channels = np.reshape(np.copy(stokes_data[0]), (stokes_data[0].shape[0]*stokes_data[0].shape[1], stokes_data[0].shape[2],stokes_data[0].shape[3]))
+    #----------------------------------------------
+    stokes_data_cnn_4channels = np.moveaxis(stokes_data_cnn_36channels, 2, 1)
+    #----------------------------------------------
+    atm_data_original = np.reshape(np.copy(atm_data[0]), (480,480,20,6))
+    atm_data_original = descale_atm(atm_data_original, phys_maxmin)
+    #----------------------------------------------
+    
+    # Model subdirectories names for the plots
+    models_names = ["simple_linear", "simple_cnn1d_36channels", "simple_cnn1d_4channels"]
+    
+    stokes_data = {
+      models_names[0]: stokes_data_linear,
+      models_names[1]: stokes_data_cnn_36channels,
+      models_names[2]: stokes_data_cnn_4channels,
+      
+    }
+    
+    #########################################################################################
+    # Models and weights paths
+    #########################################################################################
+    
+    # Setup device agnostic code
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Tensors stored in: {device}")
+    
+    # Load models
+    nn_models = {models_names[0]: SimpleLinearModel(36*4,6*20,hidden_units=2048).to(device), 
+                 models_names[1]: SimpleCNN1DModel(36,6*20,hidden_units=1024, signal_length=4).to(device),
+                 models_names[2]: SimpleCNN1DModel(4,6*20,hidden_units=72, signal_length=36).to(device),
+                 }
+    
+    models_names = list(nn_models.keys())
+    
+    # Weights paths
+    target_dir = Path("models")
+    weight_names = [models_names[0] + "_2048_hidden_units_10_epochs.pth", 
+                   models_names[1] + "_1024_hidden_units_10_epochs.pth",
+                   models_names[2] + "_72_hidden_units_10_epochs.pth",
+                   ]
+    
+    #########################################################################################
+    # Generation
+    #########################################################################################
+    
+    for i in range(len(nn_models.keys())):
+            
+      model = nn_models[models_names[i]]
+      weights_name = weight_names[i]
+      model_name = models_names[i]
+      
+      #Charge weights
+      print(f"Charging weights from {weights_name}...")
+      charge_weights(model = model,
+                      target_dir = target_dir,
+                      weights_name = weights_name
+                   )
+      
+      #Generate results
+      print(f"Generating results for {model_name}...")
+      atm_generated = generate_results(model = model,
+                                        stokes_data = stokes_data[model_name],
+                                        maxmin = phys_maxmin,
+                                        device = device
+                                      )      
+    
+      ##################################
+      # Plot generated atmospheres  
+      ##################################
+      
+      print("Plotting generated atmospheres...")
+      #Suface plots
+      plot_surface_generated_atm(
+                          atm_generated = atm_generated,
+                          atm_original = atm_data_original,
+                          model_subdir = model_name,
+                          image_name = "low_atm_surface.png",
+                          titles = mags_names,
+                          itau = 19
+                        )
+      plot_surface_generated_atm(
+                          atm_generated = atm_generated,
+                          atm_original = atm_data_original,
+                          model_subdir = model_name,
+                          image_name = "high_atm_surface.png",
+                          titles = mags_names,
+                          itau = 8
+                        )
+    
+      #OD plots
+      
+      plot_od_generated_atm(
+                        atm_generated = atm_generated,
+                        atm_original = atm_data_original,
+                        model_subdir = model_name,
+                        image_name = "mean_OD.png",
+                        titles = mags_names
+                        )
+      
+      #Density bars
+      tau_indices = range(0,20)
+      for itau in tau_indices:
+        plot_density_bars(
+                atm_generated = atm_generated,
+                atm_original = atm_data_original,
+                dense_diag_subdir= "density_plots",
+                model_subdir = model_name,
+                image_name = "OD_density.png",
+                tau_index = itau,
+                titles = mags_names,
+                num_bars = 100)
+        
+        
+      print("Done!")
+    
+if __name__ == "__main__":
+    main()
