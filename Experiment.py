6099c092a0049de8938248ec66d67954879a44da
Juan Esteban <juagudeloo@unal.edu.co>
2025-03-27 20:45:36 +0100

    temperature scaling based on the photosphere temperature range

--- Experiment.py
+++ Experiment.py
@@ -53,86 +53,51 @@ def main():
     plot_stokes(stokes=stokes_data[0], 
                 wl_points = wl_points,
                 image_name = "example_stokes",)
-    
-    #2. Create dataloaders
-    thermody_train_dataloader, thermody_test_dataloader = create_dataloaders(stokes_data = stokes_data,
-                atm_data = atm_data[...,:3],
-                device = device,
-                batch_size = 80,
-                linear = False)
-    
-    magn_train_dataloader, magn_test_dataloader = create_dataloaders(stokes_data = stokes_data,
-                atm_data = atm_data[...,3:],
-                device = device,
-                batch_size = 80,
-                linear = False)
-    
-    #3. Create models
+    #2. Create models
     scales = [1,2,4]
-    thermody_model = InversionModel(scales=scales, 
+    los_model = InversionModel(scales=scales, 
                            nwl_points=len(wl_points),
-                           n_outputs=3*len(new_logtau)).to(device).float()
-    thermody_model.name = "thermodynamic"
-    
-    magn_model = InversionModel(scales=scales, 
-                           nwl_points=len(wl_points),
-                           n_outputs=3*len(new_logtau)).to(device).float()
-    magn_model.name = "magnetic_field"
+                           n_outputs=atm_data.shape[-1]*len(new_logtau)).to(device).float()
+    los_model.name = "LOS_model"
 
-    #4. Set hyperparameters
+    #3. Set hyperparameters
     set_seeds()
     #Loss function
     loss_fn = nn.MSELoss() # this is also called "criterion"/"cost function" in some places
     #Optimizers
-    thermody_optimizer = torch.optim.Adam(params=thermody_model.parameters(), lr=lr)
-    magn_optimizer = torch.optim.Adam(params=magn_model.parameters(), lr=lr)
+    optimizer = torch.optim.Adam(params=los_model.parameters(), lr=lr)
     
-    #5. Train models
+    #4. Train models
     
-    # ----------------- Thermodynamic model -----------------
-    thermody_experiment_name = f"thermodynamic_unique"
-    print("Running experiment: ", thermody_experiment_name)
-    train(model=thermody_model,
-        train_dataloader=thermody_train_dataloader,
-        test_dataloader=thermody_test_dataloader, 
-        optimizer=thermody_optimizer,
-        loss_fn=loss_fn,
-        epochs=epochs,
-        device=device,
-        writer=create_writer(experiment_name=thermody_experiment_name,
-                            model_name=thermody_model.name,
-                            extra=f""))
-    
-    #Save the model to file so we can get back the best model
-    save_filepath = thermody_experiment_name + ".pth"
-    save_model(model=thermody_model,
-            target_dir="models",
-            model_name=save_filepath)
-    print("-"*50 + "\n")
-    
-    # ----------------- Magnetic field model -----------------
-    magn_experiment_name = f"magnetic_field_unique"
-    print("Running experiment: ", magn_experiment_name)
-    train(model=magn_model,
-        train_dataloader=magn_train_dataloader,
-        test_dataloader=magn_test_dataloader, 
-        optimizer=magn_optimizer,
-        loss_fn=loss_fn,
-        epochs=epochs,
-        device=device,
-        writer=create_writer(experiment_name=magn_experiment_name,
-                            model_name=magn_model.name,
-                            extra=f""))
-    
-    #Save the model to file so we can get back the best model
-    save_filepath = magn_experiment_name + ".pth"
-    save_model(model=magn_model,
-            target_dir="models",
-            model_name=save_filepath)
-    print("-"*50 + "\n")
-    
+    # ----------------- General model -----------------
+    experiment_name = f"only_LOS"
 
-           
+    #5. Create dataloaders
+    train_dataloader, test_dataloader = create_dataloaders(stokes_data = stokes_data,
+                atm_data = atm_data,
+                device = "cpu",  # Load data to CPU first to avoid GPU memory overflow
+                batch_size = 80,
+                linear = False)
+    
+    # ----------------- Training -----------------
+    print("Running experiment: ", experiment_name)
+    train(model=los_model,
+        train_dataloader=train_dataloader,
+        test_dataloader=test_dataloader, 
+        optimizer=optimizer,
+        loss_fn=loss_fn,
+        epochs=epochs,
+        device=device,
+        writer=create_writer(experiment_name=experiment_name,
+                            model_name=los_model.name,
+                            extra=f""))
+    
+    #Save the model to file so we can get back the best model
+    save_filepath = experiment_name + ".pth"
+    save_model(model=los_model,
+            target_dir="models",
+            model_name=save_filepath)
+    print("-"*50 + "\n")
 
 if __name__ == "__main__":
     main()
